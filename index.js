bdd02ad7836a244c0cacc7505d898c90172e601c
davidjyacus <143612899+davidjyacus@users.noreply.github.com>
2025-11-24 19:20:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -41,8 +41,8 @@ document.getElementById("get-color-scheme").addEventListener("click", function()
         /*colorDisplay[i].addEventListener("touchmove", resetTooltip);*/
         colorDisplay[i].addEventListener("touchmove", function() {
             /*reset scale and z-index in case of mobile touch*/
-            colorDisplay[i].style.scale = "1";
-            colorDisplay[i].style.zIndex = "0";
+            /*colorDisplay[i].style.scale = "1";
+            colorDisplay[i].style.zIndex = "0";*/
             const tooltip = colorDisplay[i].querySelector(".tooltiptext");
             tooltip.style.visibility = "hidden";
         });
